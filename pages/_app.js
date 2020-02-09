7a173ff6df5ff89c88d0dd62c62e155d616405db
Marissa <marissa.biesecker@gmail.com>
2020-02-09 00:09:33 +0000

    Add background and cleanup structure

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import App from 'next/app';
+import '../Styles/styles.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+);
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
+import { Background } from '../Components/Background';
+import { ThemeProvider } from 'styled-components';
+import React from 'react';
+
+const theme = {
+  flexboxgrid: {
+    breakpoints: {
+      lg: 75,
+      md: 62,
+      sm: 48,
+      xs: 0
+    },
+    container: {
+      lg: 69.25,
+      md: 62,
+      sm: 48
+    },
+    gutterWidth: 1.875
+  }
+};
+
 const Index = () => (
-  <div>
-    <p>WIP</p>
-  </div>
+  <ThemeProvider theme={theme}>
+    <>
+      <Background />
+    </>
+  </ThemeProvider>
 );
 
 export default Index;
