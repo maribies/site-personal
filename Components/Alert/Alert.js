e805f5428a3c7f46f67280cebab526b8c92c9e6f
Marissa <marissa.biesecker@gmail.com>
2020-07-23 18:17:02 +0100

    Update facets alert design and add external links

--- Components/Alert/Alert.js
+++ Components/Alert/Alert.js
@@ -1,12 +1,15 @@
 import { P } from '../Text'
-import { dropShadowWrapper, getColorValue } from '../CssHelpers'
+import { gradients, dropShadowWrapper, getColorValue } from '../CssHelpers'
 import React from 'react'
 import styled from 'styled-components'
+import { Link as BaseLink } from '../Link'
+
+const clipPath = 'polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)';
+
 
 const AlertCard = styled.div`
-  background-color: ${getColorValue('mustard')};
-  margin-bottom: 32px;
-  clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+  background-color: ${getColorValue('grey')};
+  clip-path: ${clipPath};
   width: 230px;
   height: 230px;
   display: flex;
@@ -14,13 +17,22 @@ const AlertCard = styled.div`
   flex-direction: column;
 `
 
-const Message = styled(P)`
+const AlertCardBorder = styled.div`
+  border: solid;
+  border-width: 12px;
+  border-image-slice: 1;
+  border-image-source: linear-gradient( ${gradients.one});
+  clip-path: ${clipPath};
+  background: linear-gradient(${gradients.one});
+`;
+
+export const Message = styled(P)`
   display: flex;
   flex-wrap: wrap;
   text-align: center;
   padding-left: 35px;
   padding-right: 35px;
-  color: ${getColorValue('grey')};
+  justify-content: center;
 
   &:first-child {
     margin-bottom: 0;
@@ -37,13 +49,15 @@ const Wrapper = styled(dropShadowWrapper)`
   left:  ${props => props.left};
 `
 
-export const Alert = ({ isVisible, top, left, message }) => {
+export const Alert = ({ isVisible, top, left, defaultMessage = true, message }) => {
   return isVisible && (
     <Wrapper top={top} left={left}>
-      <AlertCard>
-        <Message>Just like me, this website is a work in progress!</Message>
-        <Message>{message}</Message>
-      </AlertCard>
+      <AlertCardBorder>
+        <AlertCard>
+          {!!defaultMessage && <Message>Just like me, this website is a work in progress!</Message>}
+          <Message>{message}</Message>
+        </AlertCard>
+      </AlertCardBorder>
     </Wrapper>
   )
 }
