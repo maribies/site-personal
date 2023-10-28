7b11fda2d71ed6d4e5c913f19d08428793469889
Marissa Biescker <marissa.biesecker@gmail.com>
2023-10-28 19:04:07 +0200

    Update all the versions!

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ A personal website to reflect my personality and accomplishments.
 ## Development
 
 Developed with Next.js see documentation here: https://nextjs.org/docs/getting-started.
+
+Working locally:
+`npm run dev`
--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack']
-    })
-
-    return config
-  },
-  webpackDevMiddleware: config => {
-    // Perform customizations to webpack dev middleware config
-    return config
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
   }
+  return nextConfig
 }
