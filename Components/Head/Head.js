72feddcda10f983c5c2bf0802d541e49360cdfc8
Marissa Biescker <marissa.biesecker@gmail.com>
2023-10-29 21:11:01 +0100

    Update fonts and extend Text elements

--- Components/Head/Head.js
+++ Components/Head/Head.js
@@ -15,7 +15,7 @@ export const NextHead = () => {
       <meta name='theme-color' content='#5f4b8b' />
       <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css' />
       <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css' />
-      <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Karla|Vesper+Libre' />
+      <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=Poppins:wght@100;400&display=swap" rel="stylesheet" />
     </Head>
   )
 }
--- Components/Text/Text.js
+++ Components/Text/Text.js
@@ -2,41 +2,97 @@ import styled, { css } from 'styled-components'
 import { getColorValue } from '../CssHelpers'
 
 const baseStyles = css`
+  font-family: 'Poppins', helvetica neue, Helvetica, sans-serif;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   font-style: normal;
   color: ${getColorValue('white')};
 `
 
+const colorChange = css`
+  animation-duration: 8s;
+  animation-name: colorchange;
+  animation-iteration-count: infinite;
+  animation-direction: alternate-reverse;
+
+  @keyframes colorchange {
+    0% {
+      -webkit-text-stroke: 1px #5F4B8B;
+    }
+    20% {
+      -webkit-text-stroke: 1px #0078A7;
+    }
+    40% {
+      -webkit-text-stroke: 1px #F9AC2F;
+    }
+    60% {
+      -webkit-text-stroke: 1px #D32E5E;
+    }
+    100% {
+      -webkit-text-stroke: 1px #FF7913;
+    }
+  }
+`
+
+// Perfect Fifth
+export const Title = styled.h1`
+  font-family: 'Dela Gothic One';
+  ${baseStyles}
+  font-size: 5.063rem;
+  line-height: 6.063rem;
+  
+  margin: 0;
+
+  @media (min-width: 48rem) {
+    font-size: 7.594rem;
+    line-height: 8.594rem;
+  }
+`
+
+export const TitleOutline = styled(Title)`
+  ${colorChange}
+  -webkit-text-fill-color: transparent;
+  -webkit-text-stroke: 1px;
+`
+
 export const H1 = styled.h1`
   ${baseStyles}
-  font-size: 44px;
-  line-height: 64px;
+  font-size: 5.063rem;
+  line-height: 6.063rem;
 `
 
 export const H2 = styled.h2`
   ${baseStyles}
-  font-size: 32px;
-  line-height: 44px;
+  font-size: 3.375rem;
+  line-height: 4.375rem;
 `
 
 export const SecondaryH2 = styled(H2)`
-  font-family: 'Vesper Libre', serif;
+  font-weight: 100;
 `
 
 export const H3 = styled.h3`
   ${baseStyles}
-  font-size: 24px;
-  line-height: 32px;
+  font-size: 2.25rem;
+  line-height: 3.25rem;
 `
 
 export const SecondaryH3 = styled.h3`
-  font-family: 'Vesper Libre', serif;
+  font-weight: 100;
 `
 
+export const H3Outline = styled(H3)`
+  -webkit-text-fill-color: transparent;
+  -webkit-text-stroke: 1px;
+`
+
+export const H4 = styled.h4`
+  ${baseStyles}
+  font-size: 1.5rem;
+  line-height: 2rem;
+`
 export const P = styled.p`
   ${baseStyles}
-  font-family: 'Vesper Libre', serif;
-  font-size: 16px;
-  line-height: 24px;
+  font-size: 1rem;
+  line-height: 1.5rem;
 `
