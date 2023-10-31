91cb4dc206cd7168a9bdec97d7fd6fae21a57768
Marissa Biescker <marissa.biesecker@gmail.com>
2023-10-31 16:21:27 +0000

    Adjust connect layout

--- Components/ConnectSection/ConnectSection.js
+++ Components/ConnectSection/ConnectSection.js
@@ -13,18 +13,26 @@ const LinkInfo = styled(P)`
   padding-left: 0.5rem;
 `
 
+const LinksContainer = styled.div`
+  display: flex;
+  justify-content: end;
+  flex-grow: 2;
+`
+
 export const ConnectSection = () => {
   return (
     <Section id='connect'>
       <H2Outline>Connect</H2Outline>
 
-      {Links.map((item, index) => {
-        return (
-          <ContactLink key={index} href={item.link} className={item.icon}>
-            <LinkInfo>{item.name}</LinkInfo>
-          </ContactLink>
-        )
-      })}
+      <LinksContainer>
+        {Links.map((item, index) => {
+          return (
+            <ContactLink key={index} href={item.link} target="_blank" className={item.icon}>
+              <LinkInfo>{item.name}</LinkInfo>
+            </ContactLink>
+          )
+        })}
+      </LinksContainer>
     </Section>
   )
 }
