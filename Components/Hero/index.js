4c8979de3b5fa15f79870b59a579606abdaf0ef2
Marissa Biescker <marissa.biesecker@gmail.com>
2023-11-01 19:18:06 +0000

    Update variable names for consistency, simplicity, and understandability

--- Components/Hero/index.js
+++ Components/Hero/index.js
@@ -24,7 +24,7 @@ const NameWrapper = styled.div`
 `
 
 const ExplainLink = styled.a`
-  cursor:  ${({ active }) => active ? "url('/static/SVG/cursors/collapse.svg'), pointer" : "url('/static/SVG/cursors/question.svg'), pointer" };
+  cursor:  ${({ isActive }) => isActive ? "url('/static/SVG/cursors/collapse.svg'), pointer" : "url('/static/SVG/cursors/question.svg'), pointer" };
 `
 
 const ExplainTitle = styled(H3Outline)`
@@ -32,10 +32,10 @@ const ExplainTitle = styled(H3Outline)`
 `
 
 const ExplainText = styled(P)`
-  visibility: ${({ show }) => show ? "visible" : "hidden"};
+  visibility: ${({ isActive }) => isActive ? "visible" : "hidden"};
   margin: 0;  
-  transition: ${({ show }) => show ? "opacity 5s linear 1s" : ""};
-  opacity: ${({ show }) => show ? 1 : 0};
+  transition: ${({ isActive }) => isActive ? "opacity 5s linear 1s" : ""};
+  opacity: ${({ isActive }) => isActive ? 1 : 0};
 `
 
 const Flex = styled.div`
@@ -51,52 +51,52 @@ const FlexGrow = styled(Flex)`
 const BallArc = styled.div`
   display: flex;
   width: 30%;
-  transition: ${({ activate }) => activate ?
+  transition: ${({ isActive }) => isActive ?
     `transform-origin 5s ease-in-out,
     transform 5s ease-in-out,
     opacity 5s linear 1s` : ""};
 
-  transform-origin: ${({ activate }) => activate ? "50% 50%" : ""};
-  transform: ${({ activate }) => activate ? "rotate(180deg)" : ""};
-  opacity: ${({ activate }) => activate ? 0 : 1};
+  transform-origin: ${({ isActive }) => isActive ? "50% 50%" : ""};
+  transform: ${({ isActive }) => isActive ? "rotate(180deg)" : ""};
+  opacity: ${({ isActive }) => isActive ? 0 : 1};
 `
 
 const BallArc2 = styled(BallArc)`
   margin-left: -32px;
   width: 20%;
-  transition: ${({ activate }) => activate ?
+  transition: ${({ isActive }) => isActive ?
     `transform-origin 2s ease-in-out 5s,
     transform 2s ease-in-out 5s,
     opacity 2s linear 5s` : ""};
-  opacity: ${({ activate }) => activate ? 1 : 0};
+  opacity: ${({ isActive }) => isActive ? 1 : 0};
 `
 
 const BallArc3 = styled(BallArc2)`
   margin-left: -32px;
   width: 10%;
-  transition: ${({ activate }) => activate ?
+  transition: ${({ isActive }) => isActive ?
     `transform-origin 2s ease-in-out 7s,
     transform 2s ease-in-out 7s,
     opacity 2s linear 7s` : ""};
-  opacity: ${({ activate }) => activate ? 1 : 0 };
+  opacity: ${({ isActive }) => isActive ? 1 : 0 };
 `
 
 const SoccerBall = styled.i`
-  display: ${({ activate }) => activate ? "flex" : "none"};
+  display: ${({ isActive }) => isActive ? "flex" : "none"};
   align-self: center;
   color: #5f4b8b;
-  transition: ${({ activate }) => activate ? "transform 5s" : ""};
-  transform: ${({ activate }) => activate ? "rotate(-180deg)" : ""};
+  transition: ${({ isActive }) => isActive ? "transform 5s" : ""};
+  transform: ${({ isActive }) => isActive ? "rotate(-180deg)" : ""};
 `
 
 const SoccerBall2 = styled(SoccerBall)`
   display: flex;
-  transition: ${({ activate }) => activate ? "color 3s ease-in-out 6s, transform 2s ease-in-out 5s" : ""};
-  color: ${({ activate }) => activate ? "#2E3138" : "#5f4b8b"};
+  transition: ${({ isActive }) => isActive ? "color 3s ease-in-out 6s, transform 2s ease-in-out 5s" : ""};
+  color: ${({ isActive }) => isActive ? "#2E3138" : "#5f4b8b"};
 `
 
 const SoccerBall3 = styled(SoccerBall)`
-  transition: ${({ activate }) => activate ? "transform 2s ease-in-out 7s" : ""};
+  transition: ${({ isActive }) => isActive ? "transform 2s ease-in-out 7s" : ""};
 `
 
 export const Hero = () => {
@@ -112,22 +112,22 @@ export const Hero = () => {
     <H4>with experience through the entire product lifecycle- where business, design, user experience, and technology intersect.</H4>
     
     <Flex>
-      <ExplainLink active={isExplainLinkClicked} onClick={() => setExplainLinkClicked(!isExplainLinkClicked)}>
+      <ExplainLink isActive={isExplainLinkClicked} onClick={() => setExplainLinkClicked(!isExplainLinkClicked)}>
         <ExplainTitle>aka Utility Player</ExplainTitle>
       </ExplainLink>
       <FlexGrow>
-        <BallArc activate={isExplainLinkClicked}>
-          <SoccerBall className="fa-solid fa-futbol fa-2xl" activate={isExplainLinkClicked} />
+        <BallArc isActive={isExplainLinkClicked}>
+          <SoccerBall className="fa-solid fa-futbol fa-2xl" isActive={isExplainLinkClicked} />
         </BallArc>
-        <BallArc2 activate={isExplainLinkClicked}>
-          <SoccerBall2 className="fa-solid fa-futbol fa-2xl" activate={isExplainLinkClicked} />
+        <BallArc2 isActive={isExplainLinkClicked}>
+          <SoccerBall2 className="fa-solid fa-futbol fa-2xl" isActive={isExplainLinkClicked} />
         </BallArc2>
-        <BallArc3 activate={isExplainLinkClicked}>
-          <SoccerBall3 className="fa-solid fa-futbol fa-2xl" activate={isExplainLinkClicked} />
+        <BallArc3 isActive={isExplainLinkClicked}>
+          <SoccerBall3 className="fa-solid fa-futbol fa-2xl" isActive={isExplainLinkClicked} />
         </BallArc3>
       </FlexGrow>
     </Flex>
     
-    <ExplainText show={isExplainLinkClicked}>This title was given to me by one of my youth soccer coaches for my ability to play any position on the pitch, but excelling in defense. I have applied a similar strategy to my career talents, being proficient at technologies across the stacks and Web3, but shining on the client side.</ExplainText>
+    <ExplainText isActive={isExplainLinkClicked}>This title was given to me by one of my youth soccer coaches for my ability to play any position on the pitch, but excelling in defense. I have applied a similar strategy to my career talents, being proficient at technologies across the stacks and Web3, but shining on the client side.</ExplainText>
   </>
 }
